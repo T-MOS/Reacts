c32d22f582e793473f5f4fced72e65c7fdc81ac6
T-MOS <50530796+T-MOS@users.noreply.github.com>
2025-08-17 22:33:43 -0500

    editing player name functionality added

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,15 +1,15 @@
-import Player from "./components/player"
+import Player from "./components/Player.jsx";
 
 function App() {
-  
-  return <main>
-    <div id="game-container">
-        <ol id="players">
-            <Player name="Player 1" symbol="X" />
-            <Player name="Player 2" symbol="O" />
-        </ol>
-    </div>
-  </main>
+
+    return <main>
+        <div id="game-container">
+            <ol id="players">
+                <Player initName="Player 1" symbol="X" />
+                <Player initName="Player 2" symbol="O" />
+            </ol>
+        </div>
+    </main>
 }
 
 export default App
--- tic-tac-toe/src/components/player.jsx
+++ tic-tac-toe/src/components/player.jsx
@@ -1,11 +1,34 @@
-export default function Player({ name, symbol }) {
-  return (
-    <li>
-      <span className="player">
-        <span>{name}</span>
-        <span className="player-symbol">{symbol}</span>
-      </span>
-      <button>Edit</button>
-    </li>
-  );
+import { useState } from "react";
+
+export default function Player({ initName, symbol }) {
+    const [playerName, setPlayerName] = useState(initName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEditClick = () => {
+        setIsEditing(wasEditing => !wasEditing);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    let btnCaption = "Edit";
+
+    function handleChange(e) {
+        const name = e.target.value;
+        setPlayerName(name);
+    }
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
+        btnCaption = "Enter";
+    }
+
+
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{btnCaption}</button>
+        </li>
+    );
 }
