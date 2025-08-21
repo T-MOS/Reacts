d41caf4358cccf2d48a474019bbfe47e9416421d
T-MOS <50530796+T-MOS@users.noreply.github.com>
2025-08-20 21:45:52 -0500

    start of UserInput component

--- finance-calc/src/App.jsx
+++ finance-calc/src/App.jsx
@@ -3,14 +3,23 @@ import UserInput from './components/UserInput.jsx';
 import { useState } from "react";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <div id="body">
-
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <div id="body">
+                <div id="user-input">
+                    <span className="input-group">
+                        <UserInput />
+                        <UserInput />
+                    </span>
+                    <div className="input-group">
+                        <UserInput />
+                        <UserInput />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
--- finance-calc/src/components/UserInput.jsx
+++ finance-calc/src/components/UserInput.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 
-export default function UserInput() {
+export default function UserInput({ label = "label" }) {
+    const [text, setText] = useState();
+
+    function handleChange(e) {
+        const fieldText = e.target.value;
+
+        setText(fieldText);
+
+    }
+
+    const inputField = <input type="number" value={text} onChange={handleChange}></input>
 
     return (
         <div id="user-input">
-
+            <span>
+                <label className="center">{label}</label>
+                <p>{inputField}</p>
+            </span>
         </div>
     );
 }
