bdb6f2997db029d5b902a623d5567ae756ccdae7
T-MOS <50530796+T-MOS@users.noreply.github.com>
2025-08-21 21:12:48 -0500

    tidy

--- finance-calc/src/components/Results.jsx
+++ finance-calc/src/components/Results.jsx
@@ -9,11 +9,11 @@ export default function Results({ inputs }) {
             <table id="result">
                 <thead>
                     <tr>
-                        <th>Year</th>
-                        <th>Investment Value</th>
-                        <th>Interest (per annum)</th>
-                        <th>Total Interest</th>
-                        <th>Invested Capital</th>
+                        <th className="center">Year</th>
+                        <th className="center">Investment Value</th>
+                        <th className="center">Interest (per annum)</th>
+                        <th className="center">Total Interest</th>
+                        <th className="center">Invested Capital</th>
 
                     </tr>
                 </thead>
