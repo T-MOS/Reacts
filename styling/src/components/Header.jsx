f650a2832b73a0aaae51ec78440df5c910222eec
T-MOS <50530796+T-MOS@users.noreply.github.com>
2025-08-24 13:23:16 -0500

    Update Header.jsx

--- styling/src/components/Header.jsx
+++ styling/src/components/Header.jsx
@@ -1,12 +1,22 @@
 import logo from '../assets/logo.png';
-import customClass from './Header.module.css';
+// import customClass from './Header.module.css';
+import { styled } from 'styled-components';
 
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+`
 export default function Header() {
   return (
-    <header className={customClass.customHeader}>
+    // <header className={customClass.customHeader}>
+    <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p className={customClass.para}>A community of artists and art-lovers.</p>
-    </header>
+      <p>A community of artists and art-lovers.</p>
+    </StyledHeader>
   );
 }
